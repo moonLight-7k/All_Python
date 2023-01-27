bae6fd29b80f0b326e206f260011cfbf6820261d
suhel khan <suhelkhan87514@gmail.com>
2023-01-27 18:31:39 +0530

    Added code

--- Projects/clock.py
+++ Projects/clock.py
@@ -1,19 +1,34 @@
+# importing whole module
 from tkinter import *
 from tkinter.ttk import *
+
+# importing strftime function to
+# retrieve system's time
 from time import strftime
 
+# creating tkinter window
 root = Tk()
-root.title("Clock")
+root.title('Clock')
+
+# This function is used to
+# display time on the label
 
 
 def time():
-    string = strftime('%H:%M:%S  %p')
-    label.config(text=string)
-    label.after(1000, time)
+    string = strftime('%H:%M:%S %p')
+    lbl.config(text=string)
+    lbl.after(1000, time)
 
 
-label = Label(root, font=("ds-digital", 80),
-              background="black", foreground="cyan")
-label.pack(anchor='centre')
+# Styling the label widget so that clock
+# will look more attractive
+lbl = Label(root, font=('calibri', 40, 'bold'),
+            background='purple',
+            foreground='white')
+
+# Placing clock at the centre
+# of the tkinter window
+lbl.pack(anchor='center')
+time()
 
 mainloop()
